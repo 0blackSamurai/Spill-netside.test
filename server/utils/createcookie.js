e4bc6e8ffc9c68c440883e4818a57bf93200e6e6
Daniel <daniel.sandanger@outlook.com>
2025-02-14 14:08:33 +0100

    fiksaen

--- server/utils/createcookie.js
+++ server/utils/createcookie.js
@@ -4,6 +4,7 @@ async function createCookie(res, jwttoken){
     res.cookie("jwt", jwttoken, {
         httpOnly: true,
         maxAge: 1000*60*60*24*5, 
+        sameSite:"lax",
         secure: process.env.NODE_ENV === "production",
       });
 }
