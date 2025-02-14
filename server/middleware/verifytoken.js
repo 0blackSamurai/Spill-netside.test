ee68061144f0dab6b85ba0bcb4f211ed130a4767
Daniel <daniel.sandanger@outlook.com>
2025-02-14 12:41:25 +0100

    fiksa

--- server/middleware/verifytoken.js
+++ server/middleware/verifytoken.js
@@ -3,43 +3,40 @@ require("dotenv").config();
 const User = require("../models/userschema.js");
 
 async function verifytoken(req, res, next) {
-    try {
+//   try {
+    const jsonwebtoken = req.cookies.jwt;
 
-        const jsonwebtoken = req.cookies.jwt;
-        
-    await jwt.verify(jsonwebtoken, process.env.SECRETJWT,(async(err, decoded) => {
-        
-        if(err){
-            
-            console.log(err)
-            res.status(401).send({msg: "Unauthorized"})
+    await jwt
+      .verify(jsonwebtoken, process.env.SECRETJWT, async (err, decoded) => {
+        if (err) {
+          console.log(err);
+          res.status(401).send({ msg: "Unauthorized" });
         }
         req.user = decoded;
+
+        console.log(req.user);
         let email = decoded.email;
-        
+
         try {
-            const user = await User.findOne({ email });
-            req.user.id = user._id;
-            console.log(user,"USER ");
-            
+          const user = await User.findOne({ email });
+          req.user.id = user._id;
+          console.log(user, "USER ");
         } catch (error) {
-            console.log(error)
-            res.status(500).send({msg: "Internal server error"})
-            return;
-            
+          console.log(error);
+          res.status(500).send({ msg: "Internal server error" });
+          return;
         }
-        console.log("RUNNING AFTER TRYCATCH")
-    })).then(() => {
-        console.log("RUNNING THEN")
+        console.log("RUNNING AFTER TRYCATCH");
+      })
+      .then(() => {
+        console.log("RUNNING THEN");
         next();
         console.log(next);
-    
-    })
-    
-}catch (error){
-    console.log(error)
-    return res.status(500).send({msg:"help me"})
+      });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).send({ msg: "help me" });
+//   }
 }
-};
 
 module.exports = verifytoken;
